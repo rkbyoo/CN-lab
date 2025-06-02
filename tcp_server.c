5fb339c4dd53aa5d3da883e2913496459ad51d10
Rakib Hussain <147051779+rkbyoo@users.noreply.github.com>
2025-06-02 19:45:37 +0530

    Create tcp_server.c

---
+++ tcp_server.c
@@ -0,0 +1,72 @@
+// server.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 🔧 Fix: Allow address reuse
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind to port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port %d...\n", PORT);
+
+    // Accept client
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    if (new_socket < 0) {
+        perror("Accept failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive message
+    read(new_socket, buffer, BUFFER_SIZE);
+    printf("Client: %s\n", buffer);
+
+    // Send response
+    char *response = "I got your message";
+    send(new_socket, response, strlen(response), 0);
+    printf("Response sent to client.\n");
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
