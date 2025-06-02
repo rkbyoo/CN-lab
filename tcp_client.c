7a08086e76d1084bce24a137a478a9410d0eb2cf
Rakib Hussain <147051779+rkbyoo@users.noreply.github.com>
2025-06-02 19:43:21 +0530

    Create tcp_client.c

---
+++ tcp_client.c
@@ -0,0 +1,56 @@
+// client.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    char message[BUFFER_SIZE];
+
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IP from text to binary
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address / Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter a message to send to the server: ");
+    fgets(message, BUFFER_SIZE, stdin);
+    message[strcspn(message, "\n")] = 0; // remove newline
+
+    // Send message
+    send(sock, message, strlen(message), 0);
+
+    // Receive reply
+    read(sock, buffer, BUFFER_SIZE);
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
+
+//gcc tcp_client.c -o tcp_client
+// ./tcp_client
